ac6b34d04781902e72e7f7c0fdcd3a4d41d076ed
Stijepo Vrdoljak <stijepo.vrdoljak@rise-world.com>
2023-01-16 13:20:24 +0100

    simplify the demo

--- src/main/java/jackson/Controller.java
+++ src/main/java/jackson/Controller.java
@@ -1,7 +1,13 @@
 package jackson;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Post;
+import jakarta.inject.Inject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -12,9 +18,17 @@ public class Controller {
 
     private static final Logger log = LogManager.getLogger();
 
-    @Post("/hi")
-    public Map<String, Map<String, MyDto>> getMyDto(@Body Map<String, Map<String, MyDto>> myDto) {
-        log.info("CONTROLLER LOG: {}", myDto);
-        return myDto;
+    @Post("/mnDeserialization")
+    public Map<String, Map<String, String>> mnDeserialization(@Body Map<String, Map<String, String>> payload) {
+        log.info("CONTROLLER LOG: {}", payload);
+        return payload;
+    }
+
+    @Post("/manualDeserialization")
+    public Map<String, Map<String, String>> manualDeserialization(@Body String payload) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        var parsed = mapper.readValue(payload, new TypeReference<Map<String, Map<String, String>>>() {});
+        log.info("CONTROLLER LOG: {}", parsed);
+        return parsed;
     }
 }
--- src/test/java/jackson/JacksonTest.java
+++ src/test/java/jackson/JacksonTest.java
@@ -1,13 +1,10 @@
 package jackson;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.rxjava3.http.client.Rx3HttpClient;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
@@ -16,47 +13,40 @@ import java.util.concurrent.TimeUnit;
 @MicronautTest
 class JacksonTest {
 
-    private static final Logger log = LogManager.getLogger();
-
-    //Map<String, Map<String, MyDto>>
-    private String PAYLOAD = """
-            {
-                "firstKey": {
-                    "secondKey": null,
-                    "thirdKey": {
-                        "name": "Jackson"
-                    }
+    private static final String PAYLOAD = """
+                {
+                    "rootKey": {
+                        "key1":"value1",
+                        "key2": null
+                    }                   
                 }
-            }
-            """;
+                """;
 
     @Inject
     @Client("/endpoint")
     Rx3HttpClient client;
 
-    @Inject
-    ObjectMapper mapper;
-
-    /*
-    * Micronaut client logs are set to TRACE.
-    * REQUEST BODY can be seen in the logs after you execute the test below (same as the PAYLOAD String above).
-    * The logged out output of the controller is:
-    *
-    * Actual: CONTROLLER LOG: {firstKey={thirdKey=MyDto{name='Jackson'}}}
-    *
-    * Expected: CONTROLLER LOG: {firstKey={secondKey: null, thirdKey=MyDto{name='Jackson'}}}
-    *
-    * The "secondKey" is completely missing after deserialization which is really important for our use-case.
-    * 
-    * What am I missing?
-    * */
     @Test
-    void testJacksonDeserializationWithHashMapValueNullButKeyIsSet() {
-        var payload = HttpRequest.POST("/hi", PAYLOAD);
+    void mnDeserializtion() {
+        var payload = HttpRequest.POST("/mnDeserialization", PAYLOAD);
         client.exchange(payload, Map.class).test().awaitDone(5, TimeUnit.SECONDS).assertNoErrors().assertValue(response -> {
-            var myDtoResponse = response.body();
-            log.info("HTTP response: {}", myDtoResponse);
-           return true;
+            // DOES NOT WORK
+            // Controller output:
+            // ACTUAL: {rootKey={key1=value1}}
+            // EXPECTED: {rootKey={key1=value1, key2=null}}
+            return true;
+        });
+    }
+
+    @Test
+    void manualDeserialization() {
+        var payload = HttpRequest.POST("/manualDeserialization", PAYLOAD);
+        client.exchange(payload).test().awaitDone(5, TimeUnit.SECONDS).assertNoErrors().assertValue(response -> {
+            // WORKS
+            // Controller output
+            // ACTUAL: {rootKey={key1=value1, key2=null}}
+            // EXPECTED: {rootKey={key1=value1, key2=null}}
+            return true;
         });
     }
 
